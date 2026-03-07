a6b129b025474a99c3c59de6d1f508f825643601
M Eshwar <meshwar0696@gmail.com>
2026-03-07 11:22:52 +0530

    Time: 24 ms (26.66%), Space: 76.8 MB (89.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length == 2){
-            if(nums[0] == nums[1]){
-                return true;
-            }
-        }
+        // if(nums.length == 2){
+        //     if(nums[0] == nums[1]){
+        //         return true;
+        //     }
+        // }
         Arrays.sort(nums);
         for(int i = 0; i < nums.length-1; i++){
             if(nums[i] == nums[i+1]){
